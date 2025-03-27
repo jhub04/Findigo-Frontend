67d32b711c05dab96dfd62b44d2c7132486b6f36
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2025-03-27 14:38:13 +0100

    feat: implement home, login and register views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,48 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+
+import { useTokenStore } from '@/stores/token.ts'
+import { onMounted, ref } from 'vue';
+import userApi from '@/services/userApi.ts'
+import type { User } from '@/types/CalculationDTO.ts'
+
+const tokenStore = useTokenStore();
+const user = ref<User | null>(null);
+const isLoading = ref<boolean>(true);
+const error = ref<boolean>(false);
+
+onMounted(async () => {
+  if (tokenStore.jwtToken) {
+    try {
+      const userInfo = await userApi.getCurrentUser();
+      if (userInfo) {
+        user.value = userInfo;
+      }
+    } catch (e) {
+      error.value = true;
+    }
+  }
+  isLoading.value = false;
+});
+
 </script>
 
 <template>
-    <p>This is the homeview</p>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+      <div v-if="user">
+        <h2>Welcome, {{user.username}}</h2>
+      </div>
+      <div v-else>
+        <h2 v-if="error">Error</h2>
+        <h2 v-else>Unauthorized!</h2>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
