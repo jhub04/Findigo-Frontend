079fabfb12cd9bb09e2860eb4f0c69b652283d9d
aryma <arymalekian@gmail.com>
2025-03-28 11:17:58 +0100

    Add MapWrapper class to components

---
+++ src/components/MapWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <GMapMap :center="center" :zoom="zoom" style="width: 100%; height: 100%" />
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { GMapMap } from '@fawmi/vue-google-maps'
+
+const props = defineProps<{
+  center: { lat: number; lng: number }
+  zoom: number
+}>()
+</script>
