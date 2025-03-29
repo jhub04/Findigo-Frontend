7583dedd6079c4037182d16dd13cc664afb75697
aryma <arymalekian@gmail.com>
2025-03-29 12:49:12 +0100

    Add all listings to GMap

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -1,40 +1,40 @@
-  <template>
+<template>
   <GMapMap class="google-map" :center="center" :zoom="zoom">
-  <GMapMarker
-    v-for="listing in categoryListings"
-    :key="listing.id"
-    :position="{ lat: listing.latitude, lng: listing.longitude }"
-  />
+    <GMapMarker
+      v-for="listing in allListings"
+      :key="listing.id"
+      :position="{ lat: listing.latitude, lng: listing.longitude }"
+    />
   </GMapMap>
-  </template>
+</template>
 
-  <script setup lang="ts">
-  import { ref, onMounted } from 'vue'
-  import { getListingsByCategory} from '@/services/categoryApi.ts';
-  const { center, zoom } = defineProps<{
-    center: { lat: number; lng: number }
-    zoom: number
-  }>()
-  const categoryListings = ref<ListingResponse[]>([]);
-  const categoryListingsLoading = ref(true);
-  const categoryListingsError = ref<string | null>(null);
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { getAllListings } from '@/services/listingApi.ts'
+import type { ListingResponse } from '@/types/dto.ts'
 
-  onMounted(async () => {
-    try {
-      categoryListings.value = await getListingsByCategory();
-      console.log(categoryListings.value);
-    } catch (err: any) {
-      categoryListingsError.value = err.message || 'Failed to load listings';
-    } finally {
-      categoryListingsLoading.value = false;
-    }
-  });
-  </script>
+const { center, zoom } = defineProps<{
+  center: { lat: number; lng: number }
+  zoom: number
+}>()
 
-  <style scoped>
-  .google-map {
-    width: 100%;
-    height: 100%;
-    border-radius: 8px; /* valgfritt, for penere utseende */
+const allListings = ref<ListingResponse[]>([])
+const error = ref<string | null>(null)
+
+onMounted(async () => {
+  try {
+    allListings.value = await getAllListings()
+    console.log(allListings.value)
+  } catch (err: never) {
+    error.value = err.message || 'Failed to load listings'
   }
-  </style>
+})
+</script>
+
+<style scoped>
+.google-map {
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+}
+</style>
