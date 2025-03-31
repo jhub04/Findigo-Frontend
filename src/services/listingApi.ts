a7efbe77c52c8d225af48f8fdb30f68d2951a02c
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2025-03-31 13:00:15 +0200

    refactor(new listing): update new listing page

--- src/services/listingApi.ts
+++ src/services/listingApi.ts
@@ -4,10 +4,7 @@ import { useTokenStore } from '@/stores/token.ts'
 import apiClient from '@/services/apiClient.ts'
 
 export const addListing = async(data: ListingRequest): Promise<ListingResponse> => {
-  const tokenStore = useTokenStore();
-  const username = tokenStore.loggedInUser;
-
-  const response = await apiClient.post<ListingResponse>(`/listings/${username}`, data);
+  const response = await apiClient.post<ListingResponse>(`/listings`, data);
   return response.data;
 }
 
@@ -23,30 +20,10 @@ export const getAllListings = async(): Promise<ListingResponse[]> => {
 
 export const getListingById = async (id: number): Promise<ListingResponse | null> => {
   try {
-    const response = await apiClient.get<ListingResponse>(`/listings/${id}`);
+    const response = await apiClient.get<ListingResponse>(`/listings/id/${id}`);
     return response.data;
   } catch (error) {
     console.error(`Failed to fetch listing: ${error} `);
     return null;
   }
 }
-
-export const getUserListings = async(): Promise<ListingResponse[]> => {
-  const tokenStore = useTokenStore();
-  const username = tokenStore.loggedInUser;
-  const jwtToken = tokenStore.jwtToken;
-
-  if (!username || !jwtToken) {
-    throw new Error("User is not logged in or token is missing.");
-  }
-
-  try {
-    const response = await apiClient.get<ListingResponse[]>(`/listings/${username}`, {
-      headers: { Authorization: `Bearer ${jwtToken}` }
-    });
-    return response.data || [];
-  } catch (error) {
-    console.error(`Failed to fetch listings: ${error} `);
-    return [];
-  }
-};
--- src/services/userApi.ts
+++ src/services/userApi.ts
@@ -1,4 +1,6 @@
 import apiClient from '@/services/apiClient.ts'
+import type { ListingResponse } from '@/types/dto.ts'
+import { useTokenStore } from '@/stores/token.ts'
 
 export const getCurrentUser = async () => {
   try {
@@ -9,7 +11,28 @@ export const getCurrentUser = async () => {
     console.error(`Failed to fetch current user: ${error})`)
     return null
   }
-}
+};
+
+/*
+export const getUserListings = async(): Promise<ListingResponse[]> => {
+  const tokenStore = useTokenStore();
+  const username = tokenStore.loggedInUser;
+  const jwtToken = tokenStore.jwtToken;
+
+  if (!username || !jwtToken) {
+    throw new Error("User is not logged in or token is missing.");
+  }
+
+  try {
+    const response = await apiClient.get<ListingResponse[]>(`/listings/${username}`, {
+      headers: { Authorization: `Bearer ${jwtToken}` }
+    });
+    return response.data || [];
+  } catch (error) {
+    console.error(`Failed to fetch listings: ${error} `);
+    return [];
+  }
+}; */
 
 const userApi = {
   getCurrentUser,
