492987283bb8b2ed2f3cd65c39ff5cb7e9edff83
aryma <arymalekian@gmail.com>
2025-03-31 13:30:36 +0200

    Add route to searchresults page

---
+++ src/views/SearchResultsView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <!-- Viser "Loading..." mens loading er true -->
+    <div v-if="loading">Loading...</div>
+
+    <!-- Når loading er ferdig, vises søkeresultatene i en liste -->
+    <ul v-else>
+      <!-- Går gjennom hvert resultat og viser det i et <li> -->
+      <li v-for="r in results" :key="r.id">{{ r.title }}</li>
+    </ul>
+  </div>
+</template>
