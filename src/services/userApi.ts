0d6583edd24da81086c51105c919f1bf36eb143b
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2025-03-31 20:03:09 +0200

    refactor(listing link): pointer quick fix

--- src/services/userApi.ts
+++ src/services/userApi.ts
@@ -13,27 +13,6 @@ export const getCurrentUser = async () => {
   }
 };
 
-/*
-export const getUserListings = async(): Promise<ListingResponse[]> => {
-  const tokenStore = useTokenStore();
-  const username = tokenStore.loggedInUser;
-  const jwtToken = tokenStore.jwtToken;
-
-  if (!username || !jwtToken) {
-    throw new Error("User is not logged in or token is missing.");
-  }
-
-  try {
-    const response = await apiClient.get<ListingResponse[]>(`/listings/${username}`, {
-      headers: { Authorization: `Bearer ${jwtToken}` }
-    });
-    return response.data || [];
-  } catch (error) {
-    console.error(`Failed to fetch listings: ${error} `);
-    return [];
-  }
-}; */
-
 const userApi = {
   getCurrentUser,
 }
