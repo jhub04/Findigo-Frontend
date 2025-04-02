3e53c6a3a6480b10cecbf5ed70fb5d0003b1f5cb
aryma <arymalekian@gmail.com>
2025-04-02 14:25:41 +0200

    Make SearchResultsView.vue parent to FilterSidebar

---
+++ src/components/search/SearchResultsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class='temporary'>
+    <FilterSideBar v-model:selectedCategory="selectedCategory" v-model:searchQuery="searchQuery" />
+    <SearchListings :selectedCategory="selectedCategory" :searchQuery="searchQuery" />
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import FilterSideBar from '@/components/search/FilterSideBar.vue'
+import SearchListings from '@/components/search/SearchListings.vue'
+
+
+const selectedCategory = ref<number | 'all'>('all')
+const searchQuery = ref('')
+
+
+</script>
+
+<style scoped>
+.temporary {
+  display: flex;
+  flex-direction: row;
+  gap: 500px;
+}
+</style>
+
+
