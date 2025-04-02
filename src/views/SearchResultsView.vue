bab5d4139305bfcc50b62145f5432b8c0dcbaeb2
aryma <arymalekian@gmail.com>
2025-04-02 11:07:13 +0200

    Add flex to SearchResultsView for layout testing

--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
+import FilterCategory from '@/components/search/FilterCategory.vue'
 import SearchResults from '@/components/search/SearchListings.vue'
 </script>
 
 <template>
-  <SearchResults />
+  <div class='temporary'>
+    <FilterCategory />
+    <SearchResults />
+  </div>
+
 </template>
+
+<style scoped>
+.temporary {
+  display: flex;
+  flex-direction: row;
+  gap: 100px;
+}
+</style>
+
+
