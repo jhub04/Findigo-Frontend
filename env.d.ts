a44d9e302882aa43fd317eff1ab528df1243dba9
aryma <arymalekian@gmail.com>
2025-04-02 11:52:53 +0200

    fix

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,19 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_MAPS_API_KEY: string;
+  readonly BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
 declare global {
   interface Window {
     google: typeof google;
