b588dbfc8f3ef2f63f18f50bc02006a395e14cb2
jonathan <jonathhu@stud.ntnu.no>
2025-04-05 12:57:59 +0200

    Fix navbar

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,14 +1,22 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
-import { login, clearCookie } from '@/services/authApi.ts';
+import { login, clearCookie, isAuthenticated } from '@/services/authApi.ts';
 
 export const useUserStore = defineStore("token", () => {
   const loggedInUser = ref<string | null>(sessionStorage.getItem("loggedInUser"));
+  const authenticated = ref(false);
+
+  const initializeAuthStatus = async () => {
+    authenticated.value = await isAuthenticated();
+  };
+
+  initializeAuthStatus();
 
   const loginAndSaveUser = async (username: string, password: string) => {
     await login(username, password); //This can throw error
     loggedInUser.value = username;
     sessionStorage.setItem("loggedInUser", username);
+    authenticated.value = await isAuthenticated();
   };
 
   const logout = async () => {
@@ -21,9 +29,10 @@ export const useUserStore = defineStore("token", () => {
   
     // After logout request, redirect to login page
     window.location.href = "/login";
+    authenticated.value = false;
     loggedInUser.value = null;
     sessionStorage.clear();
   };
 
-  return { loggedInUser, loginAndSaveUser, logout };
+  return { loggedInUser, authenticated, loginAndSaveUser, logout };
 });
