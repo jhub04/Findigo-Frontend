abb7f793a507816ece9c8dc94740c340f39af2c2
jonathan <jonathhu@stud.ntnu.no>
2025-04-09 17:51:51 +0200

    Fix baseURL

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useUserStore } from '@/stores/user'
 
 const apiClient = axios.create({
-  baseURL: 'https://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true
 });
 
