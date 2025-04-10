96b8d53d1e5db552aa6880e3ee39fc31a107937f
Scott <scottld@stud.ntnu.no>
2025-04-10 22:32:27 +0200

    fix message

--- cypress/e2e/messages/messages.cy.ts
+++ cypress/e2e/messages/messages.cy.ts
@@ -2,13 +2,14 @@ describe('Messages', () => {
   beforeEach(() => {
     cy.resetDatabase()
     cy.initializeTestData()
-    cy.visit('http://localhost:5173')
+    cy.visit('http://localhost:5173/login')
 
     cy.url().should('include', '/login')
     cy.contains('Register').click()
     cy.get('input[name="username"]').type('testuser3')
     cy.get('input[name="password"]').type('1234')
-    cy.get('button[type="submit"]').click()
+    cy.get('.submit-btn').click()
+
 
     cy.url().should('include', '/register')
     cy.contains('Login').click()
@@ -38,10 +39,11 @@ describe('Messages', () => {
     cy.visit('http://localhost:5173/profile')
     cy.url().should('include', '/profile')
     cy.get('.logout-button').click()
+    cy.visit('http://localhost:5173/login')
     cy.get('input[name="username"]').type('testuser')
     cy.get('input[name="password"]').type('1234')
     cy.get('.submit-btn').click()
-    cy.get('[href="/messages"]').click()
+    cy.visit('http://localhost:5173/messages')
     cy.get('.participants').should('have.text', 'testuser3')
     cy.get('.message-body').should('have.text', 'Hei')
     cy.get('.message-preview').first().should('have.class', 'unread-message')
@@ -59,16 +61,18 @@ describe('Messages', () => {
     cy.visit('http://localhost:5173/profile')
     cy.url().should('include', '/profile')
     cy.get('.logout-button').click()
+    cy.visit('http://localhost:5173/login')
+
     cy.get('input[name="username"]').type('testuser')
     cy.get('input[name="password"]').type('1234')
     cy.get('.submit-btn').click()
-    cy.get('[href="/messages"]').click()
+    cy.visit('http://localhost:5173/messages')
     cy.get('.participants').should('have.text', 'testuser3')
     cy.get('.message-body').should('have.text', 'Hei')
     cy.get('.message-preview').first().should('have.class', 'unread-message')
 
     cy.get('.message-preview').click()
-    cy.get('[href="/messages"]').click()
+    cy.visit('http://localhost:5173/messages')
     cy.get('.message-preview').first().should('not.have.class', 'unread-message')
   })
 
@@ -80,7 +84,7 @@ describe('Messages', () => {
     cy.get('textarea').type('Hei')
     cy.get('.message-input > button').click()
 
-    cy.get('[href="/messages"]').click()
+    cy.visit('http://localhost:5173/messages')
     cy.get('.message-preview').first().should('not.have.class', 'unread-message')
   })
 })
